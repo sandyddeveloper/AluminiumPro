c713302c2d9d5a5a4730ecde9d84bb36b5dda23e
sandyddeveloper <sandyddeveloper@gmail.com>
2024-09-10 17:24:45 +0530

    updating

--- Aluminium_Pro/Aluminium_Pro/settings.py
+++ Aluminium_Pro/Aluminium_Pro/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,7 +92,7 @@ DATABASES = {
 }
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
